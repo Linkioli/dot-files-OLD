7db583507dad55b9facb9e0b43987f1b05cd08d7
Malachi <malachishaffer@protonmail.com>
2022-07-20 10:27:45 -0400

    replaced slstatus config

--- .config/slstatus/config.def.h
+++ .config/slstatus/config.def.h
@@ -45,7 +45,6 @@ static const char unknown_str[] = "n/a";
  * ram_total           total memory size in GB         NULL
  * ram_used            used memory in GB               NULL
  * run_command         custom shell command            command (echo foo)
- * separator           string to echo                  NULL
  * swap_free           free swap in GB                 NULL
  * swap_perc           swap usage in percent           NULL
  * swap_total          total swap size in GB           NULL
@@ -59,18 +58,10 @@ static const char unknown_str[] = "n/a";
  * uptime              system uptime                   NULL
  * username            username of current user        NULL
  * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
- *                                                     NULL on OpenBSD
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { disk_perc, "[DISK %.5s%%]", "/" },
-    { wifi_essid, "[%s ", "iwn0" },
-    { wifi_perc, "%3s%%]", "iwn0" },
-  	{ battery_perc, "[BAT %3s%%]", "BAT1" },
-    { cpu_perc, "[CPU %3s%%]", NULL    },    
-    { ram_perc, "[RAM %2s%%]", NULL    },
-    { swap_perc, "[SWAP %2s%%]", NULL    },
-	{ datetime, "[%s]",           "%a %e %b %k:%M" }
+	{ datetime, "%s",           "%F %T" },
 };
